7a33ced1c4516d925b6f0cbdc725960c97f0a323
Harry M Liversedge <liversedge@LAPTOP-1TVSG7CF.localdomain>
2019-07-28 13:02:45 +0100

    renamed file from getStarted to HomePage and home within django as this was something as a new starter that caused confusion

--- backend/uclapi/dashboard/views.py
+++ backend/uclapi/dashboard/views.py
@@ -165,7 +165,7 @@ def dashboard(request):
 
 
 @ensure_csrf_cookie
-def get_started(request):
+def home(request):
     logged_in = True
 
     try:
@@ -176,7 +176,7 @@ def get_started(request):
     articles = get_articles()
     token = get_temp_token()
 
-    return render(request, 'getStarted.html', {
+    return render(request, 'HomePage.html', {
         'initial_data': {
             'temp_token': token,
             'logged_in': str(logged_in),
